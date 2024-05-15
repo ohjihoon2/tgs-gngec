a2c4d81e715775aebffc58602c7e98c593349b63
ohjihoon2 <38777474+ohjihoon2@users.noreply.github.com>
2024-05-15 23:54:38 +0900

    test code 추가

--- src/test/java/io/tgsinc/tgsgngec/controller/domain/admin/controller/AdmTestControllerTest.java
+++ src/test/java/io/tgsinc/tgsgngec/controller/domain/admin/controller/AdmTestControllerTest.java
@@ -1,13 +1,30 @@
 package io.tgsinc.tgsgngec.controller.domain.admin.controller;
 
+import io.tgsinc.tgsgngec.domain.admin.service.AdminService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
+@SpringBootTest
 class AdmTestControllerTest {
+
+    @Autowired
+    AdminService adminService;
 
 
     @Test
     void test(){
 
+        Map<String, Object> paramMap = new HashMap<>();
+
+        paramMap.put("id", "tset1");
+        paramMap.put("pswd", "1234");
+        paramMap.put("role", "ADMIN");
+
+        adminService.addAdmin(paramMap);
     }
 }
