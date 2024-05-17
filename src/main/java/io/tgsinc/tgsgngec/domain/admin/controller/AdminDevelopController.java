08960af9dd43695d1af5cd7f0145ef4614fe4cd4
ohjihoon2 <38777474+ohjihoon2@users.noreply.github.com>
2024-05-17 09:41:45 +0900

    연구개발 controller 진행중

--- src/main/java/io/tgsinc/tgsgngec/domain/admin/controller/AdminDevelopController.java
+++ src/main/java/io/tgsinc/tgsgngec/domain/admin/controller/AdminDevelopController.java
@@ -35,22 +35,19 @@ public class AdminDevelopController {
 
 
     /**
-     * @methodName adminDevelop
+     * @methodName showAdminResearchList
      * @author ohjihoon
      * @date 2024-05-16 오전 11:24
      * @description 연구개발 연구과제 list
      */
     @GetMapping("/research")
-    public String adminDevelop(@PageableDefault(page = 0, size = 10, sort = "idx", direction = Sort.Direction.ASC) Pageable pageable,
+    public String showAdminResearchList(@PageableDefault(page = 0, size = 10, sort = "idx", direction = Sort.Direction.ASC) Pageable pageable,
                                @ModelAttribute ResearchSearch search, Model model){
 
         //entity를 외부에 노출 X
         //Parameter도 DTO로 받을껀데 이름은 Form 으로 만들고, response 할때는 DTO 로 반환
 
         Page<RsrchAsmtDTO> resultList = rsrchAsmtService.findAll(search, pageable);
-        System.out.println("resultList = " + resultList);
-        System.out.println("resultList.getContent() = " + resultList.getContent());
-        System.out.println("resultList.getContent().toString() = " + resultList.getContent().toString());
         model.addAttribute("resultList", resultList);
         model.addAttribute("search", search);
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
@@ -63,13 +60,13 @@ public class AdminDevelopController {
     }
 
     /**
-     * @methodName adminDevelopDetail
+     * @methodName showAdminResearchDetail
      * @author ohjihoon
      * @date 2024-05-16 오후 4:43
      * @description 연구개발 연구과제 상세
      */
     @GetMapping("/research/{idx}")
-    public String adminDevelopDetail(@PathVariable int idx, Model model){
+    public String showAdminResearchDetail(@PathVariable int idx, Model model){
 
         model.addAttribute("","");
 
@@ -77,20 +74,26 @@ public class AdminDevelopController {
     }
 
     //TODO 연구개발 연구과제 등록
+    @GetMapping("/research/{idx}")
+    public String showAdminResearchInsert(){
+        return "";
+    }
+
+
     //TODO 연구개발 연구과제 삭제
     //TODO 연구개발 연구과제 수정
 
 
 
     /**
-     * @methodName adminDevelopApply
+     * @methodName showAdminDevelopApply
      * @author ohjihoon
      * @date 2024-05-16 오후 4:35
      * @description 연구개발 원문 신청조회 list
      */
 
     @GetMapping("/aply")
-    public String adminDevelopApply(){
+    public String showAdminDevelopApply(){
         return "/page/admin/develop/original-apply/index";
     }
 }
